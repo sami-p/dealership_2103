63d6cacc336fc63a896fb5aba53a8c99faefc1eb
sami-p <sami.peterson14@gmail.com>
2021-03-31 16:56:22 -0500

    Add dealership class

---
+++ lib/dealership.rb
@@ -0,0 +1,19 @@
+class Dealership
+  attr_reader :name,
+              :address,
+              :inventory
+
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+  end
+
+  def inventory_count
+    @inventory.length
+  end 
+
+  def add_car(car)
+    inventory << car
+  end
+end
