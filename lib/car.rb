5c26b55a7b265c3dea4462e69232140b3863e16d
sami-p <sami.peterson14@gmail.com>
2021-05-02 17:52:01 -0500

    Add #total_cost, #@color, and #paint!; pass Iteration 1 specs

--- lib/car.rb
+++ lib/car.rb
@@ -2,7 +2,8 @@ class Car
   attr_reader :make,
               :model,
               :monthly_payment,
-              :loan_length
+              :loan_length,
+              :color
 
   def initialize(make_model, monthly_payment, loan_length)
     @make_model = make_model
@@ -10,5 +11,14 @@ class Car
     @model = make_model.split.last
     @monthly_payment = monthly_payment
     @loan_length = loan_length
+    @color = nil
+  end
+
+  def total_cost
+    monthly_payment * loan_length
+  end
+
+  def paint!(color)
+    @color = color
   end
 end
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -28,4 +28,28 @@ RSpec.describe Car do
       expect(car.loan_length).to eq(36)
     end
   end
+
+  describe '#total_cost' do
+    it 'calculates total cost' do
+      car = Car.new("Ford Mustang", 1500, 36)
+
+      expect(car.total_cost).to eq(54000)
+    end
+  end
+
+  describe '#color' do
+    it 'defaults color to nil' do
+      car = Car.new("Ford Mustang", 1500, 36)
+
+      expect(car.color).to eq(nil)
+    end
+
+    it 'can paint car and change color' do
+      car = Car.new("Ford Mustang", 1500, 36)
+
+      car.paint!(:blue)
+
+      expect(car.color).to eq(:blue)
+    end
+  end
 end
